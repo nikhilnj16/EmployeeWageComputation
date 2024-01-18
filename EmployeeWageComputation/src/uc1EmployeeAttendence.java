30878bbb9739711ff73ccfd56a1a4bbfcf6358cc
Nikhil <nikiljana123@gmail.com>
2024-01-18 17:00:53 +0530

    Initial/main commit

---
+++ EmployeeWageComputation/src/uc1EmployeeAttendence.java
@@ -0,0 +1,14 @@
+public class uc1EmployeeAttendence {
+    public static void main(String[] args) {
+        int is_full_time = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == is_full_time){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+    }
+}
+
+
