96a1a3c130f02262458c0a6999a27afd88b028cb
Nikhil <nikiljana123@gmail.com>
2024-01-18 17:38:29 +0530

    parttimewage

--- EmployeeWageComputation/src/uc1EmployeeAttendence.java
+++ EmployeeWageComputation/src/uc1EmployeeAttendence.java
@@ -1,12 +1,16 @@
 public class uc1EmployeeAttendence {
     public static void main(String[] args) {
-        int is_full_time = 1;
+        int is_full_time = 2;
+        int is_part_time = 1;
         int emp_rate_per_hour = 20;
         int empHrs = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == is_full_time){
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == is_part_time){
+            empHrs = 4;
+        }
+        else if (empCheck == is_full_time){
             empHrs = 8;
         }
         else {
