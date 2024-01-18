c84f452d35af8153c39f0c38b04b23c75b3a53ee
Nikhil <nikiljana123@gmail.com>
2024-01-18 20:40:46 +0530

    monthwagecondition

--- EmployeeWageComputation/src/uc1EmployeeAttendence.java
+++ EmployeeWageComputation/src/uc1EmployeeAttendence.java
@@ -3,11 +3,13 @@ public class uc1EmployeeAttendence {
     public static final int is_full_time = 2;
     public static final int emp_rate_per_hour = 20;
     public static final int num_of_working_days = 20;
+    public static final int max_hrs_in_month = 100;
 
     public static void main(String[] args) {
 
-        int empHrs = 0, empWage = 0, totalEmpWage = 0;
-        for (int day = 0; day < num_of_working_days; day++) {
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        while (totalEmpHrs <= max_hrs_in_month && totalWorkingDays < num_of_working_days) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case is_part_time:
@@ -19,10 +21,10 @@ public class uc1EmployeeAttendence {
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * emp_rate_per_hour;
-            totalEmpWage += empWage;
-            
+            totalEmpHrs += empHrs;
+            System.out.println("Day#: " + totalWorkingDays + " Emp hr: " + empHrs);
         }
+        int totalEmpWage = totalEmpHrs * emp_rate_per_hour;
         System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 
