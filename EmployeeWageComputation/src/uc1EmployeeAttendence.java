a40150c66d4bc06b4366f2d06f51460fd05d910a
Nikhil <nikiljana123@gmail.com>
2024-01-18 20:29:13 +0530

    monthwage

--- EmployeeWageComputation/src/uc1EmployeeAttendence.java
+++ EmployeeWageComputation/src/uc1EmployeeAttendence.java
@@ -2,26 +2,30 @@ public class uc1EmployeeAttendence {
     public static final int is_part_time = 1;
     public static final int is_full_time = 2;
     public static final int emp_rate_per_hour = 20;
+    public static final int num_of_working_days = 20;
+
     public static void main(String[] args) {
 
-        int empHrs = 0;
-        int empWage = 0;
-
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck){
-            case is_part_time:
-            empHrs = 4;
-            break;
-            case is_full_time:
-            empHrs = 8;
-            break;
-            default:
-                empHrs = 0;
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        for (int day = 0; day < num_of_working_days; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case is_part_time:
+                    empHrs = 4;
+                    break;
+                case is_full_time:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * emp_rate_per_hour;
+            totalEmpWage += empWage;
+            
         }
-        empWage = empHrs * emp_rate_per_hour;
-        System.out.println("Emp Wage: " + empWage);
-
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
+
 }
 
 
