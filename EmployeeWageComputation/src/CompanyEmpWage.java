8fee396cbd34dac623ad91d526f547aa948c9c94
Nikhil <nikiljana123@gmail.com>
2024-01-24 11:58:52 +0530

    added: ManageWageMultiCompy

---
+++ EmployeeWageComputation/src/CompanyEmpWage.java
@@ -0,0 +1,22 @@
+public class CompanyEmpWage {
+    public final String company;
+    public final int emp_rate_per_hour;
+    public final int num_of_working_days;
+    public final int max_hrs_in_month;
+    public int totalEmpWage;
+
+    public CompanyEmpWage(String company, int emp_rate_per_hour, int num_of_working_days, int max_hrs_in_month) {
+        this.company = company;
+        this.emp_rate_per_hour = emp_rate_per_hour;
+        this.num_of_working_days = num_of_working_days;
+        this.max_hrs_in_month = max_hrs_in_month;
+    }
+    public void setTotalEmpWage(int totalEmpWage){
+        this.totalEmpWage = totalEmpWage;
+    }
+
+    @Override
+    public String toString() {
+        return "Total Emp Wage for Company: " +company+ " is: " + totalEmpWage;
+    }
+}
--- EmployeeWageComputation/src/uc1EmployeeAttendence.java
+++ EmployeeWageComputation/src/uc1EmployeeAttendence.java
@@ -2,23 +2,28 @@ public class uc1EmployeeAttendence {
     public static final int is_part_time = 1;
     public static final int is_full_time = 2;
 
-    private final String company;
-    private final int emp_rate_per_hour;
-    private final int num_of_working_days;
-    private final int max_hrs_in_month;
-    private int totalEmpWage;
+    private int numOfCompany = 0;
+    private CompanyEmpWage[] companyEmpWageArray;
 
-    public uc1EmployeeAttendence(String company, int emp_rate_per_hour, int num_of_working_days, int max_hrs_in_month) {
-        this.company = company;
-        this.emp_rate_per_hour = emp_rate_per_hour;
-        this.num_of_working_days = num_of_working_days;
-        this.max_hrs_in_month = max_hrs_in_month;
+    public uc1EmployeeAttendence() {
+        companyEmpWageArray = new CompanyEmpWage[5];
     }
 
-    public void computeEmpWage() {
+    private void addCompanyEmpWage(String company, int emp_rate_per_hour, int num_of_working_days, int max_hrs_in_month){
+        companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, emp_rate_per_hour, num_of_working_days, max_hrs_in_month);
+        numOfCompany++;
+    }
+
+    private void computeEmpWage(){
+        for (int i = 0; i < numOfCompany; i++){
+            companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
+            System.out.println(companyEmpWageArray[i]);
+        }
+    }
+    private int computeEmpWage(CompanyEmpWage companyEmpWage) {
 
         int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-        while (totalEmpHrs <= max_hrs_in_month && totalWorkingDays < num_of_working_days) {
+        while (totalEmpHrs <= companyEmpWage.max_hrs_in_month && totalWorkingDays < companyEmpWage.num_of_working_days) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -34,21 +39,16 @@ public class uc1EmployeeAttendence {
             totalEmpHrs += empHrs;
             System.out.println("Day#: " + totalWorkingDays + " Emp hr: " + empHrs);
         }
-        totalEmpWage = totalEmpHrs * emp_rate_per_hour;
+        return totalEmpHrs * companyEmpWage.emp_rate_per_hour;
     }
 
-    @Override
-    public String toString() {
-        return "Total Emp Wage for Company: " +company+ " is: " + totalEmpWage ;
-    }
+
 
     public static void main(String[] args) {
-        uc1EmployeeAttendence dMart = new uc1EmployeeAttendence("DMart", 20, 20, 100);
-        uc1EmployeeAttendence jio = new uc1EmployeeAttendence("Jio", 15, 23, 100);
-        dMart.computeEmpWage();
-        System.out.println(dMart);
-        jio.computeEmpWage();
-        System.out.println(jio);
+        uc1EmployeeAttendence empWage = new uc1EmployeeAttendence();
+        empWage.addCompanyEmpWage("Jio", 15, 23, 100);
+        empWage.addCompanyEmpWage("DMart", 20, 20, 100);
+        empWage.computeEmpWage();
     }
 
 }
