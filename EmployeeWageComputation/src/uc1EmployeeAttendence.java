cf02b9fad555a4e4344a7dec038dba89c753665e
Nikhil <nikiljana123@gmail.com>
2024-01-24 10:12:37 +0530

    added: classmethod

--- EmployeeWageComputation/src/uc1EmployeeAttendence.java
+++ EmployeeWageComputation/src/uc1EmployeeAttendence.java
@@ -5,7 +5,7 @@ public class uc1EmployeeAttendence {
     public static final int num_of_working_days = 20;
     public static final int max_hrs_in_month = 100;
 
-    public static void main(String[] args) {
+    public static void computeEmpWage() {
 
         int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
         while (totalEmpHrs <= max_hrs_in_month && totalWorkingDays < num_of_working_days) {
@@ -26,6 +26,11 @@ public class uc1EmployeeAttendence {
         }
         int totalEmpWage = totalEmpHrs * emp_rate_per_hour;
         System.out.println("Total Emp Wage: " + totalEmpWage);
+
+    }
+
+    public static void main(String[] args) {
+        computeEmpWage();
     }
 
 }
